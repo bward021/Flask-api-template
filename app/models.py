68338e737a079e66fa675cf5f884987207424c5f
Brandon Ward <brandonward.gk@gmail.com>
2021-04-02 09:25:34 -0600

    updated models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from functools import cache
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
@@ -8,6 +9,7 @@ class User(db.Model):
     username = db.Column(db.String(100), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True, cascade="all, delete-orphan, delete")
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -18,3 +20,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.Title)
